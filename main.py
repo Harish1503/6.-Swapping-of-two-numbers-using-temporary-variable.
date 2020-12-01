11dc909bf772d9602041577b30ae5bb54d087082
Harish Kumar <n.harishkumar1503@gmail.com>
2020-12-01 08:33:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Before Swapping")
+print("a is", a)
+print("b is", b)
+temp = a
+a = b
+b = temp
+print("After Swapping")
+print("a is", a)
+print("b is", b)
